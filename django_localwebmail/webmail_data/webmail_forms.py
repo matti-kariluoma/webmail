cf87b30ebad556d9a9d38067bb892cf44f516ae4
Matti Kariluoma, NDSU <matti.m.kariluoma@ndsu.edu>
2011-12-24 16:37:38 -0600

    making the to, cc, bcc subject lines more readable

--- django_localwebmail/webmail_data/webmail_forms.py
+++ django_localwebmail/webmail_data/webmail_forms.py
@@ -5,9 +5,9 @@ class LoginForm(forms.Form):
 	password = forms.CharField(widget=forms.PasswordInput(render_value=False))
 
 class ComposeForm(forms.Form):
-	to = forms.CharField()
-	cc = forms.CharField()
-	bcc = forms.CharField()
-	subject = forms.CharField()
+	to = forms.CharField(attrs={'size': '80'})
+	cc = forms.CharField(attrs={'size': '80'})
+	bcc = forms.CharField(attrs={'size': '80'})
+	subject = forms.CharField(attrs={'size': '80'})
 	message = forms.CharField(widget=forms.widgets.Textarea(attrs={'cols':'80', 'rows':'15'}))
 	
