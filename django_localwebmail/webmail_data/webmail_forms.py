34cd528fda931fbb37be240b6d2bda1d63955cd1
Matti Kariluoma, NDSU <matti.m.kariluoma@ndsu.edu>
2011-12-24 19:00:10 -0600

    did the atrr={} wrong

--- django_localwebmail/webmail_data/webmail_forms.py
+++ django_localwebmail/webmail_data/webmail_forms.py
@@ -5,9 +5,9 @@ class LoginForm(forms.Form):
 	password = forms.CharField(widget=forms.PasswordInput(render_value=False))
 
 class ComposeForm(forms.Form):
-	to = forms.CharField(attrs={'size': '80'})
-	cc = forms.CharField(attrs={'size': '80'})
-	bcc = forms.CharField(attrs={'size': '80'})
-	subject = forms.CharField(attrs={'size': '80'})
+	to = forms.CharField(widget=forms.TextInput(attrs={'size':'80'}))
+	cc = forms.CharField(widget=forms.TextInput(attrs={'size':'80'}))
+	bcc = forms.CharField(widget=forms.TextInput(attrs={'size':'80'}))
+	subject = forms.CharField(widget=forms.TextInput(attrs={'size':'80'}))
 	message = forms.CharField(widget=forms.widgets.Textarea(attrs={'cols':'80', 'rows':'15'}))
 	
